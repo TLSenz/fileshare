6b5dce60d03258a763a7fbc9e3c8a68ffc55b7e5
Sven <svenzemp@proton.me>
2025-09-04 17:55:01 +0200

    New Files

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+
+
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system;};
+
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+            buildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.openssl
+            pkgs.pkg-config
+            pkgs.docker-compose
+            pkgs.sqlx-cli
+            ];
+
+          };
+      };
+
+  }
+
