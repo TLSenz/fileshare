d41211111ec24703bfa05278d39205d097b2f575
Sven <sven@zemp.email>
2026-02-08 16:23:58 +0100

    healthchek improvement

--- src/controller/health_check.rs
+++ src/controller/health_check.rs
@@ -1,8 +1,9 @@
 use axum::http::status;
+use axum::Json;
 use axum::response::IntoResponse;
 
 #[tracing::instrument]
 pub async fn health_check() -> impl IntoResponse {
     tracing::debug!("Health check called");
-    status::StatusCode::OK
+    Json("Healthy")
 }
